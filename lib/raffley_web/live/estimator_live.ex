11dd3173c45b9ed2f28a6d264b50e34a61750305
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-18 14:10:10 -0300

    feat(main): Update tickets quantity by 10 every 2 seconds.

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -2,6 +2,10 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   def mount(_params, _session, socket) do #controller pass a conn struct and liveview a socket struct
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     socket = assign(socket, tickets: 0, price: 3)
 
     {:ok, socket}
@@ -22,5 +26,10 @@ defmodule RaffleyWeb.EstimatorLive do
     socket = assign(socket, :price, String.to_integer(price))
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, update(socket, :tickets, &(&1 + 10))}
   end
 end
