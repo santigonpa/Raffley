692ef3eb3419ff4602316ab01415d453ced7bcc8
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-19 12:07:12 -0300

    feat(main): Use function components and attr declarations.

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -10,22 +10,36 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def render(assigns) do
     ~H"""
-      <div class="raffle-index">
-        <div class="raffles">
-          <div :for={raffle <- @raffles} class="card">
-            <img src={raffle.image_path}/>
-            <h2><%= raffle.prize %></h2>
-            <div class="details">
-              <div class="price">
-                $<%= raffle.ticket_price %> / ticket
-              </div>
-              <div class="badge">
-                <%= raffle.status %>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="raffle-index">
+      <div class="raffles">
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle}/>
       </div>
+    </div>
     """
   end
-end
+
+  attr :raffle, Raffley.Raffley
+  def raffle_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@raffle.image_path}/>
+      <h2><%= @raffle.prize %></h2>
+      <div class="details">
+        <div class="price">
+          $<%= @raffle.ticket_price %> / ticket
+        </div>
+        <.badge status={@raffle.status}/>
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      <%= @status %>
+    </div>
+    """
+  end
+ end
