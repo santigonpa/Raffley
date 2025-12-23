48db06d17d54dc8c358a834b352ec0d7fc78c113
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-23 11:06:29 -0300

    feat(main/24): Filter Form.

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -5,7 +5,11 @@ defmodule RaffleyWeb.RaffleLive.Index do
   import RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
-    socket = stream(socket, :raffles, Raffles.list_raffles())
+    socket =
+      socket
+      |> stream(:raffles, Raffles.list_raffles())
+      |> assign(:form, to_form(%{}))
+
     {:ok, socket}
   end
 
@@ -22,6 +26,9 @@ defmodule RaffleyWeb.RaffleLive.Index do
           Any guesses?
         </:details>
       </.banner>
+
+      <.filter_form form={@form} />
+
       <div class="raffles" id="raffles" phx-update="stream">
         <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id} />
       </div>
@@ -49,5 +56,23 @@ defmodule RaffleyWeb.RaffleLive.Index do
     """
   end
 
-
+  def filter_form(assigns) do
+    ~H"""
+      <.form for={@form}>
+        <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
+        <.input
+          type="select"
+          field={@form[:status]}
+          prompt="Status"
+          options={[:upcoming, :open, :closed]}
+        />
+        <.input
+          type="select"
+          field={@form[:sort_by]}
+          prompt="Sort By"
+          options={[:price, :ticket_price]}
+        />
+      </.form>
+    """
+  end
 end
