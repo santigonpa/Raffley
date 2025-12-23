f8ac5f54d96e3ee2f3142b6da2d4fa3a3f83ce17
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-23 10:00:31 -0300

    feat(main/23): Ecto Queries.

--- lib/raffley/raffles.ex
+++ lib/raffley/raffles.ex
@@ -1,5 +1,6 @@
 alias Raffley.Raffles.Raffle
 alias Raffley.Repo
+import Ecto.Query
 
 defmodule Raffley.Raffles do
   def list_raffles do
@@ -11,6 +12,19 @@ defmodule Raffley.Raffles do
   end
 
   def featured_raffles(raffle) do
-    list_raffles() |> List.delete(raffle)
+    Raffle
+    |> where(status: :open)
+    |> where([r], r.id != ^raffle.id)
+    |> order_by(desc: :ticket_price)
+    |> limit(3)
+    |> Repo.all()
+  end
+
+  def filter_raffles do
+    Raffle
+    |> where(status: :closed)
+    |> where([r], ilike(r.prize, "%gourmet%"))
+    |> order_by(:prize)
+    |> Repo.all()
   end
 end
