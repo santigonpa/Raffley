1f31e6e76d9c2a5c1ba20ac9c2354eb445804e04
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-29 09:29:58 -0300

    feat(main/34): Forms With Changesets.

--- lib/raffley/raffles/raffle.ex
+++ lib/raffley/raffles/raffle.ex
@@ -6,8 +6,8 @@ defmodule Raffley.Raffles.Raffle do
     field :status, Ecto.Enum, values: [:upcoming, :open, :closed]
     field :description, :string
     field :prize, :string
-    field :ticket_price, :integer
-    field :image_path, :string
+    field :ticket_price, :integer, default: 1
+    field :image_path, :string, default: "/images/placeholder.jpg"
 
     timestamps(type: :utc_datetime)
   end
@@ -17,5 +17,7 @@ defmodule Raffley.Raffles.Raffle do
     raffle
     |> cast(attrs, [:prize, :description, :ticket_price, :status, :image_path])
     |> validate_required([:prize, :description, :ticket_price, :status, :image_path])
+    |> validate_length(:description, min: 10)
+    |> validate_number(:ticket_price, greater_than_or_equal_to: 1)
   end
 end
--- lib/raffley_web/live/admin_raffle_live/form.ex
+++ lib/raffley_web/live/admin_raffle_live/form.ex
@@ -2,37 +2,40 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
   use RaffleyWeb, :live_view
 
   alias Raffley.Admin
+  alias Raffley.Raffles.Raffle
 
   def mount(_params, _session, socket) do
+    changeset = Raffle.changeset(%Raffle{}, %{})
+
     socket =
       socket
       |> assign(:page_title, "New Raffle")
-      |> assign(:form, to_form(%{}, as: "raffle"))
+      |> assign(:form, to_form(changeset))
 
-      {:ok, socket}
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
     <.header>
-      <%= @page_title %>
+      {@page_title}
     </.header>
     <.simple_form for={@form} id="raffle-form" phx-submit="save">
-      <.input field={@form[:prize]} label="Prize"/>
+      <.input field={@form[:prize]} label="Prize" />
 
-      <.input field={@form[:description]} type="textarea" label="Description"/>
+      <.input field={@form[:description]} type="textarea" label="Description" />
 
-      <.input field={@form[:ticket_price]} type="number" label="Ticket price"/>
+      <.input field={@form[:ticket_price]} type="number" label="Ticket price" />
 
       <.input
         field={@form[:status]}
         type="select"
         label="Status"
         prompt="Choose a status"
-        options = {[:upcoming, :open, :closed]}
+        options={[:upcoming, :open, :closed]}
       />
 
-      <.input field={@form[:image_path]} label="Image Path"/>
+      <.input field={@form[:image_path]} label="Image Path" />
 
       <:actions>
         <.button phx-disable-with="Saving...">Save Raffle</.button>
@@ -46,10 +49,19 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
   end
 
   def handle_event("save", %{"raffle" => raffle_params}, socket) do
-    _raffle = Admin.create_raffle(raffle_params)
+    case Admin.create_raffle(raffle_params) do
+      {:ok, _raffle} ->
+        socket =
+          socket
+          |> put_flash(:info, "Raffle created successfully!")
+          |> push_navigate(to: ~p"/admin/raffles")
 
-    socket = push_navigate(socket, to: ~p"/admin/raffles")
+        {:noreply, socket}
 
-    {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = assign(socket, :form, to_form(changeset))
+
+        {:noreply, socket}
+    end
   end
 end
