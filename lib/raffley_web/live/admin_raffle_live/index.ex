cfcf2fd43fdb51ca8d16b3831c9441fa387941db
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2025-12-29 13:53:35 -0300

    feat(main/38): Delete Raffle.

--- lib/raffley_web/live/admin_raffle_live/index.ex
+++ lib/raffley_web/live/admin_raffle_live/index.ex
@@ -17,7 +17,7 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
     ~H"""
     <div class="admin-index">
       <.header>
-        <%= @page_title %>
+        {@page_title}
         <:actions>
           <.link navigate={~p"/admin/raffles/new"} class="button">
             New Raffle
@@ -27,22 +27,34 @@ defmodule RaffleyWeb.AdminRaffleLive.Index do
       <.table id="raffles" rows={@streams.raffles}>
         <:col :let={{_dom_id, raffle}} label="Prize">
           <.link navigate={~p"/raffles/#{raffle}"}>
-            <%= raffle.prize %>
+            {raffle.prize}
           </.link>
         </:col>
         <:col :let={{_dom_id, raffle}} label="Status">
           <.badge status={raffle.status} />
         </:col>
         <:col :let={{_dom_id, raffle}} label="Ticket Price">
-          <%= raffle.ticket_price %>
+          {raffle.ticket_price}
         </:col>
         <:action :let={{_dom_id, raffle}}>
           <.link navigate={~p"/admin/raffles/#{raffle}/edit"}>
             Edit
           </.link>
         </:action>
+        <:action :let={{_dom_id, raffle}}>
+          <.link phx-click="delete" phx-value-id={raffle.id} data-confirm="Are you sure?">
+            Delete
+          </.link>
+        </:action>
       </.table>
     </div>
     """
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    raffle = Admin.get_raffle!(id)
+    {:ok, _} = Admin.delete_raffle(raffle)
+
+    {:noreply, stream_delete(socket, :raffles, raffle)}
   end
 end
