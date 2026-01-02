f687a47d97d8b75f22e405a1a077a3014be01525
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2026-01-02 11:57:25 -0300

    feat(main/46): Generate the Auth System.

--- mix.exs
+++ mix.exs
@@ -32,7 +32,9 @@ defmodule Raffley.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.21"},
+      # {:phoenix, github: "phoenixframework/phoenix", ref: "bd589f1", override: true},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
